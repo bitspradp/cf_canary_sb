82ec255f99547eb27ed1231c22c0676d7d12268c
bitspradp <trust.pradeep@gmail.com>
2019-01-21 18:08:18 +0530

    Update version_verify.sh

--- tests/it/version_verify.sh
+++ tests/it/version_verify.sh
@@ -3,7 +3,7 @@ canary_version=$(grep version /tests/version.py | cut -d \" -f 2)
 version=test
 count=100
 while [ $canary_version != $version ];
- do version=$(curl http://ornithology:5000/version)
+ do version=$(curl http://demoapp:5000/version)
  echo canary is $canary_version
  echo version is $version
  sleep 1
