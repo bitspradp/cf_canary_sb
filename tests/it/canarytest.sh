53da8c2a8209281f211dbcaf2692862ddcbfeaf7
bitspradp <trust.pradeep@gmail.com>
2019-01-21 18:53:04 +0530

    Update canarytest.sh

--- tests/it/canarytest.sh
+++ tests/it/canarytest.sh
@@ -7,7 +7,7 @@ while [ $i -ge 0  ];
 do
   i=$(( $i - 1 ))
   echo i is $i
-  curl --connect-timeout 20 http://${APP_URL}:5000/ping
+  curl --connect-timeout 20 mongodb://${APP_URL}:5000/ping
 done
 if [ $? -ne 0 ]; then
   exit 1
